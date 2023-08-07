3fe1a0cf304e31133896f255f8721daa48d49d84
shinji.shichijo <shichijoshinji@yamatoku.jp>
2023-08-07 02:39:32 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -34,7 +34,14 @@ class TasksController extends Controller
     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリテーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -69,9 +76,15 @@ class TasksController extends Controller
     // putまたはpatchでtasks/ (任意のid)にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+        
         //idの値でタスクを検索して収得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
