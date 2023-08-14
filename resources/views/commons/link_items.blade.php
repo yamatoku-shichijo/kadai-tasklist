7be6f0fcd529446d364229a334380ecec85f92ff
shinji.shichijo <shichijoshinji@yamatoku.jp>
2023-08-14 07:21:06 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{-- タスク一覧ページへのリンク --}}
+    <li><a class="link link-hover" href="#">Tasks</a></li>
+    {{-- タスク詳細ページへのリンク --}}
+    <li><a class="link link-hover" href="#">{{ Auth::task()->name }}&#39;s </a>detail</li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
